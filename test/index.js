4f2292b288156f9dd665f7ded19096c88d69dbfa
Yash Kumar <tyushk@gmail.com>
2022-01-30 11:16:15 -0600

    Add testfile

---
+++ test/index.js
@@ -0,0 +1,14 @@
+// note that this is clearly not actual js, and just exists as a test file for snitch
+// todo: make this compile
+
+// todoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo: fix capitalization (pass)
+
+// todo: add another m to hmmm (pass)
+const hmmm = (yes) => { this.clearly() ? isValid() : javascript(yes) };
+
+class ToDo {
+    constructor(deez) {
+        deez.nuts()
+        this.status = "gottem"
+    }
+}
